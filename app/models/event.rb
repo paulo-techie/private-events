6dc52f3c39cba931901f29eba0d756d58f81effc
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-05 05:41:07 +0300

    add attendees to event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  # belongs_to :user
+
+  has_many :event_attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendances, source: :attendee
 end
