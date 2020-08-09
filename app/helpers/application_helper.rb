a2ed335f1a5f4d7becd068d3f4e71e8dd6242dae
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-09 08:06:37 +0300

    fix lint error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def navbar_brand
     if current_user
-      content_tag(:p, current_user.name, class: "navbar-brand disabled m-0 p-0")
+      content_tag(:p, current_user.name, class: 'navbar-brand disabled m-0 p-0')
     else
-      content_tag(:p, 'Login/Signup', class: "navbar-brand disabled m-0 p-0")
+      content_tag(:p, 'Login/Signup', class: 'navbar-brand disabled m-0 p-0')
     end
   end
 end
